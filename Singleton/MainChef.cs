22310cf6e7f7f4c963624d9280d4586ce51aa792
first <Katya_Klebleeva@mail.ru>
2023-11-27 09:50:57 +0300

    added a Singleton example

---
+++ Singleton/MainChef.cs
@@ -0,0 +1,25 @@
+﻿namespace Singleton;
+
+public class MainChef
+{
+    static MainChef instance;
+
+    protected MainChef()
+    {
+        
+    }
+
+    public static MainChef GetMainChef()
+    {
+        if (instance == null)
+        {
+            instance = new MainChef();
+        }
+        return instance;
+    }
+    public void PrepareDish(string dishName)
+    {
+        Console.WriteLine("Chef is preparing dish: " + dishName);
+    }
+
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using Singleton;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        MainChef chef1 = MainChef.GetMainChef();
+        MainChef chef2 = MainChef.GetMainChef();
+        MainChef chef3 = MainChef.GetMainChef();
+        MainChef chef4 = MainChef.GetMainChef();
+
+        if (chef1 == chef2 && chef2 == chef3 && chef3 == chef4)
+        {
+            Console.WriteLine("Same instance\n");
+        }
+        chef1.PrepareDish("Lasagna");
+        chef2.PrepareDish("Roast goose"); 
+        chef3.PrepareDish("Sirloin steak "); 
+        chef4.PrepareDish("Goulash"); 
+    }
+}
